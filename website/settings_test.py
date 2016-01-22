2ac27da53580656c9c97309a9e2e02868842bde0
Alexander Schrijver <alex@flupzor.nl>
2016-01-23 00:27:14 +0100

    Add database for tests.

--- website/settings_test.py
+++ website/settings_test.py
@@ -1,1 +1,12 @@
+import os
+
 from settings_base import *
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.dirname(APP_ROOT)+'/newsdiffs.db',
+    }
+}
+
