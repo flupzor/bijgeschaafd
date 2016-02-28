a6f2035d8e6c1809f4eefe1879c830cd7065cf43
Alexander Schrijver <alex@flupzor.nl>
2016-02-28 23:00:15 +0100

    Rewrite the test to make it less markup-dependent.

--- news/tests/test_views.py
+++ news/tests/test_views.py
@@ -2,6 +2,8 @@ from datetime import timedelta, datetime
 
 from django.core.urlresolvers import reverse
 from django.utils import timezone
+from django.utils.formats import localize
+from django.utils.timezone import localtime
 from django_webtest import WebTest
 
 from .factory_models import ArticleFactory, VersionFactory
@@ -66,72 +68,62 @@ class ViewTests(WebTest):
 
         response = self.app.get(reverse('browse'))
         rows = response.pyquery.find('table tbody tr')
-        table_matrix = [
-            {
-                "column1": rows.eq(i).find('td').eq(0).html(),
-                "column2": rows.eq(i).find('td').eq(1).html(),
-                "column3": rows.eq(i).find('td').eq(2).html()
-            } for i in range(len(rows))
-        ]
 
-        expected = [{
-            'column1': '\n          <a href="{}">article1 revision1</a> '
-                       '(<a href="{}">None</a>)\n          <br/>\n        '.format(
-                           reverse(
-                               'article_history',
-                               args=[article1.filename(), ]
-                           ),
-                           article1.url
-                       ),
-            'column2': '25 januari 2015 06:00',
-            'column3': '<a href="{}">(Vergelijk)</a>'.format(
-                reverse(
-                    'diffview',
-                    kwargs={
-                        'vid1': article1_version2.pk,
-                        'vid2': article1_version1.pk,
-                        'urlarg': article1.filename()
-                    }
-                )),
-        }, {
-            'column1': '25 januari 2015 05:00',
-            'column2': '<a href="{}">(Vergelijk)</a>'.format(
-                reverse(
-                    'diffview',
-                    kwargs={
-                        'vid1': article1_version3.pk,
-                        'vid2': article1_version2.pk,
-                        'urlarg': article1.filename()
-                    }
-                )),
-            'column3': None,
-        }, {
-            'column1': '24 januari 2015 05:00',
-            'column2': None,
-            'column3': None,
-        }, {
-            'column1': '\n          <a href="{}">article2 revision1</a> '
-                       '(<a href="{}">None</a>)\n          <br/>\n        '.format(
-                           reverse(
-                               'article_history',
-                               args=[article2.filename(), ]
-                           ),
-                           article2.url
-                       ),
-            'column2': '25 januari 2015 05:00',
-            'column3': '<a href="{}">(Vergelijk)</a>'.format(
-                reverse(
-                    'diffview',
-                    kwargs={
-                        'vid1': article2_version2.pk,
-                        'vid2': article2_version1.pk,
-                        'urlarg': article2.filename()
-                    }
-                )),
-        }, {
-            'column1': '25 januari 2015 04:00',
-            'column2': None,
-            'column3': None,
-        }]
+        def assert_article_row(row, article, version1, version2):
+            column1 = row.find('td').eq(0)
 
-        self.assertEquals(table_matrix, expected)
+            article_link = column1.find('a').eq(0).attr('href')
+            article_title = column1.find('a').eq(0).text()
+            expected_article_link = reverse(
+                'article_history', args=[article.filename(), ])
+            self.assertEquals(article_link, expected_article_link)
+            self.assertEquals(article_title, article.latest_version().title)
+
+            column2 = row.find('td').eq(1)
+            self.assertEquals(
+                column2.text(),
+                localize(localtime(article.latest_version().date))
+            )
+
+            self.assertEquals(version1, article.version_set.all()[0])
+            self.assertEquals(version2, article.version_set.all()[1])
+
+            column3 = row.find('td').eq(2)
+            expected_diff_link = reverse(
+                'diffview', kwargs={
+                    'vid1': version2.pk,
+                    'vid2': version1.pk,
+                    'urlarg': article.filename()
+                })
+            diff_link = column3.find('a').attr('href')
+            self.assertEquals(diff_link, expected_diff_link)
+
+        def assert_version_row(row, version1, version2):
+            column1 = row.find('td').eq(0)
+            self.assertEquals(
+                column1.text(),
+                localize(localtime(version1.date))
+            )
+
+            if version2:
+                column2 = row.find('td').eq(1)
+                expected_diff_link = reverse(
+                    'diffview', kwargs={
+                        'vid1': version2.pk,
+                        'vid2': version1.pk,
+                        'urlarg': article1.filename()
+                    })
+                diff_link = column2.find('a').attr('href')
+
+                self.assertEquals(expected_diff_link, diff_link)
+
+        assert_article_row(
+            rows.eq(0), article1, article1_version1, article1_version2)
+        assert_version_row(
+            rows.eq(1), article1_version2, article1_version3)
+        assert_version_row(
+            rows.eq(2), article1_version3, None)
+        assert_article_row(
+            rows.eq(3), article2, article2_version1, article2_version2)
+        assert_version_row(
+            rows.eq(4), article2_version2, None)
