3d3b670a0d963e03b03eda2f1888907f8931a9d9
Alexander Schrijver <alex@flupzor.nl>
2016-04-03 22:14:27 +0200

    This should lower the memory consumption.

--- news/migrations/0013_migrate_titles.py
+++ news/migrations/0013_migrate_titles.py
@@ -4,12 +4,36 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 from django.db.models import F
 
+
+# Copied from: https://djangosnippets.org/snippets/1170/
+def batch_qs(qs, batch_size=1000):
+    """
+    Returns a (start, end, total, queryset) tuple for each batch in the given
+    queryset.
+
+    Usage:
+        # Make sure to order your querset
+        article_qs = Article.objects.order_by('id')
+        for start, end, total, qs in batch_qs(article_qs):
+            print "Now processing %s - %s of %s" % (start + 1, end, total)
+            for article in qs:
+                print article.body
+    """
+    total = qs.count()
+    for start in range(0, total, batch_size):
+        end = min(start + batch_size, total)
+        yield (start, end, total, qs[start:end])
+
+
 def migrate_titles(apps, schema_editor):
     Version = apps.get_model("news", "Version")
 
-    for version in Version.objects.filter(content__startswith=F('title')):
-        version.content = version.content.replace(version.title, '').lstrip()
-        version.save()
+    versions = Version.objects.filter(content__startswith=F('title'))
+    for start, end, total, qs in batch_qs(versions):
+        print "Now processing %s - %s of %s" % (start + 1, end, total)
+        for version in qs:
+            version.content = version.content.replace(version.title, '').lstrip()
+            version.save()
 
 
 class Migration(migrations.Migration):
--- news/migrations/_migrate_dates.py
+++ news/migrations/_migrate_dates.py
@@ -2,6 +2,26 @@ from news.dateparsers import (
     NosNLDateParser, NuNLDateParser, TelegraafDateParser, MetroDateParser)
 
 
+# Copied from: https://djangosnippets.org/snippets/1170/
+def batch_qs(qs, batch_size=1000):
+    """
+    Returns a (start, end, total, queryset) tuple for each batch in the given
+    queryset.
+
+    Usage:
+        # Make sure to order your querset
+        article_qs = Article.objects.order_by('id')
+        for start, end, total, qs in batch_qs(article_qs):
+            print "Now processing %s - %s of %s" % (start + 1, end, total)
+            for article in qs:
+                print article.body
+    """
+    total = qs.count()
+    for start in range(0, total, batch_size):
+        end = min(start + batch_size, total)
+        yield (start, end, total, qs[start:end])
+
+
 def migrate_dates(apps, schema_editor, source):
     date_parser_mapping = {
         'nos.nl': NosNLDateParser,
@@ -15,24 +35,27 @@ def migrate_dates(apps, schema_editor, source):
     versions = Version.objects.filter(article__source=source)
     total = versions.count()
     failures = 0
-    for version in versions:
-        parser = date_parser_mapping[version.article.source]
+    for start, end, total, qs in batch_qs(versions):
+        print "Now processing %s - %s of %s" % (start + 1, end, total)
 
-        content = version.content
-        try:
-            newline_position = content.find('\n')
-            firstline = content[:newline_position]
-        except Exception:
-            pass
+        for version in qs:
+            parser = date_parser_mapping[version.article.source]
 
-        try:
-            # Store the date as a datetime in the database.
-            version.modified_date_in_article = \
-                parser.process(firstline)
-            # Remove the date in the content.
-            version.content = version.content[newline_position+1:]
-            version.save(update_fields=['modified_date_in_article', 'content'])
-        except ValueError:
-            failures += 1
+            content = version.content
+            try:
+                newline_position = content.find('\n')
+                firstline = content[:newline_position]
+            except Exception:
+                pass
 
-#    print "Failures {}/{}".format(failures, total)
+            try:
+                # Store the date as a datetime in the database.
+                version.modified_date_in_article = \
+                    parser.process(firstline)
+                # Remove the date in the content.
+                version.content = version.content[newline_position+1:]
+                version.save(update_fields=['modified_date_in_article', 'content'])
+            except ValueError:
+                failures += 1
+
+    #    print "Failures {}/{}".format(failures, total)
